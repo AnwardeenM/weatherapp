0f37e412bb42fc8d632396350f99f2394d9ad172
AnwardeenM <kalilanwar597@gmail.com>
2022-11-30 22:22:36 +0530

    Working: The input taking part is done and work going for dynamic states

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const autoCompleteURL ="https://api.weatherapi.com/v1/search.json?key=94fa33516b0e48c2a67170420222911&q="
+
 function App() {
+  const [city,setCity]= useState("");
+  const [clicked,setClicked] = useState(false);
+  const [citysuggestion,setCitySuggestion] = useState([]);
+
+
+  const handleclick=(clickedcity)=>{
+    console.log(clickedcity)
+    setCity(clickedcity);
+   setClicked(true);
+    // setCitySuggestion([]);
+  }
+
+  useEffect(()=>{
+    const fetchcitysuggestion = async()=>{
+      const res = await fetch(autoCompleteURL +city);
+      const data = await res.json();
+      const citySuggestionData = data.map(curdata=>`${curdata.name}, ${curdata.region}, ${curdata.country}`)
+      setCitySuggestion(citySuggestionData);
+
+    }
+    if(!clicked && city.length>2){
+      fetchcitysuggestion();
+    }
+    else{
+      setCitySuggestion([]);
+      setClicked(false)
+    }
+    
+  },[city])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header"><b>Anwardeen's Weather Report</b></div>
+        <div className='app_body'>
+          <input type="text" className='cityTextbox' placeholder='Enter the City name'
+          onChange={(event)=>setCity(event.target.value)}  value={city}>
+            </input>
+            {citysuggestion.length>0 && (
+              <div className='suggestionWrapper'>
+              {citysuggestion.map(curCity=>(
+                <div className='suggestion' onClick={()=>handleclick(curCity)}>{curCity}</div>
+              ))}
+              </div>
+            )}
+            
+        </div>      
     </div>
   );
 }
